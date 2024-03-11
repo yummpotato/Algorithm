e2050453ee664e20340dac51656eb25d11f659e4
감자 <hevlyoy26@naver.com>
2024-03-11 17:44:49 +0900

    [level 0] Title: 두 수의 차, Time: 0.01 ms, Memory: 59.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        var answer: Int = 0
+        if (num1 >= -50000 && num1 <= 50000) {
+            if (num2 >= -50000 && num2 <= 50000) {
+                answer = num1 - num2
+            }
+        }
+        return answer
+    }
+}
