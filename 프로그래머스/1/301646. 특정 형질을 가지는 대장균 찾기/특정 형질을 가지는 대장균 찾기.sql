1dfd44b5b115e33822bfa0426b7f8f0c4e4513b8
박혜정 <hevlyoy26@naver.com>
2024-10-29 11:10:43 +0900

    Update 특정 형질을 가지는 대장균 찾기.sql

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -7,3 +7,11 @@
 SELECT COUNT(ID) as COUNT
 FROM ECOLI_DATA
 WHERE BIN(GENOTYPE) LIKE '%101' OR BIN(GENOTYPE) LIKE '%100' OR BIN(GENOTYPE) LIKE '%001' OR BIN(GENOTYPE) = 1
+
+-- 최적 연산
+-- & 연산자: 비트 단위 AND 연산
+-- 2(10)와 & 연산한 것이 2가 아님 => 즉, 두 번째 비트가 설정되지 않은 경우에만 참
+-- 5(101)와 & 연산한 것이 0이 아님 => 즉, 첫 번째 비트나 세 번째 비트가 설정된 경우에만 참
+SELECT COUNT(*) COUNT
+FROM ECOLI_DATA
+WHERE GENOTYPE & 2 != 2 AND GENOTYPE & 5 != 0
